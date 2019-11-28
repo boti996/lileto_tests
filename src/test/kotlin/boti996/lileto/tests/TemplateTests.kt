18946406ff0d940daa413cd71381cb4110f66545
boti996 <dobai.botond@gmail.com>
2019-11-28 03:33:31 +0100

    template tests: new test-cases were added
    Test-cases were added where we are loading data via containers into templates and return their text value.

--- src/test/kotlin/boti996/lileto/tests/TemplateTests.kt
+++ src/test/kotlin/boti996/lileto/tests/TemplateTests.kt
@@ -6,9 +6,16 @@ import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import kotlin.test.assertEquals
 
-private val templatePlaceholder = "template bracket"
-private val templateWithSlotPlaceholder = "template bracket with slot {<=slot>} "
+private const val templatePlaceholder = "template bracket"
+private const val templateWithSlotPlaceholder = "template bracket with slot [ {<=slot>} ]"
+private const val templateWith3SlotsPlaceholder = "template bracket with slots [ {</slot1>} ] [ {</slot2>} ] [ {</slot3>} ]"
+private const val evaluatedTemplateWith3SlotsChunk = "template bracket with slots"
+private const val slotPlaceholder = "slot"
+private const val evaluatedTemplateWithSlot = "template bracket with slot [$slotPlaceholder]"
+private const val evaluatedTemplateWithUnfilledSlot = "template bracket with slot []"
 private const val emptinessInMySoul = ""
+
+private fun addContanerElement(element: Any) = "${BracketType.TEXT.open()}$element${BracketType.TEXT.close()} "
 
 class TemplateTests : Spek({
 
@@ -43,9 +50,56 @@ class TemplateTests : Spek({
             nonEvaluatedTemplateInPlaintext(
                 templatePlaceholder,
                 emptinessInMySoul
+            ),
+
+            evaluatedTemplateInPlaintext_withSlot(
+                templateWithSlotPlaceholder,
+                slotPlaceholder,
+                evaluatedTemplateWithSlot
+            ),
+
+            evaluatedTemplateInPlaintext_withUnfilledSlot(
+                templateWithSlotPlaceholder,
+                evaluatedTemplateWithUnfilledSlot
+            ),
+
+            multipleSlotsInTemplate(
+                templateWith3SlotsPlaceholder,
+                BracketType.CONTAINER.open() +
+                        "slot1:=${addContanerElement("first")}" +
+                        "slot2:=${addContanerElement("second")}" +
+                        "slot3:=${addContanerElement("third")}" +
+                        BracketType.CONTAINER.close(),
+                "$evaluatedTemplateWith3SlotsChunk [first] [second] [third]"
+            ),
+
+            multipleSlotsInTemplate(
+                templateWith3SlotsPlaceholder,
+                BracketType.CONTAINER.open() +
+                        "slot1 slot2 slot3 |\n" +
+                        addContanerElement(11) +
+                        addContanerElement(12) +
+                        addContanerElement(13) +
+
+                        addContanerElement(21) +
+                        addContanerElement(22) +
+                        addContanerElement(23) +
+
+                        addContanerElement(liletoNullValue) +
+                        addContanerElement(32) +
+                        addContanerElement(liletoNullValue) +
+
+                        addContanerElement(liletoNullValue) +
+                        addContanerElement(liletoNullValue) +
+                        addContanerElement(liletoNullValue) +
+
+                        BracketType.CONTAINER.close(),
+                "$evaluatedTemplateWith3SlotsChunk [11] [12] [13]" +
+                        "$evaluatedTemplateWith3SlotsChunk [21] [22] [23]" +
+                        "$evaluatedTemplateWith3SlotsChunk [] [32] []" +
+                        "$evaluatedTemplateWith3SlotsChunk [] [] []",
+                description = listOf("Load multiple lines of data", "into template")
             )
-
-
         )
 
         // Assertions
@@ -57,6 +111,7 @@ class TemplateTests : Spek({
     }
 })
 
+//TODO: custom description
 internal fun evaluatedTemplateInPlaintext(templateContent: String,
                                           expected: String,
                                           evaluated: Boolean = true)
@@ -66,15 +121,64 @@ internal fun evaluatedTemplateInPlaintext(templateContent: String,
     return singleBracketInPlaintext(
         BracketWithContent(
             BracketType.COMMAND,
-            " template = ${BracketType.TEMPLATE.open()}$templateContent${BracketType.TEMPLATE.close()}\n" +
-                    "output = template$evaluate "),
+    " template = ${BracketType.TEMPLATE.open()}$templateContent${BracketType.TEMPLATE.close()}\n" +
+            "output = template$evaluate "),
         expected
     )
 }
 
+//TODO: custom description
 internal fun nonEvaluatedTemplateInPlaintext(templateContent: String,
                                           expected: String)
         : testcase {
 
     return evaluatedTemplateInPlaintext(templateContent, expected, evaluated = false)
+}
+
+//TODO: custom description
+internal fun evaluatedTemplateInPlaintext_withSlot(templateContent: String,
+                                                   slotContent: String,
+                                                   expected: String)
+        : testcase {
+
+    return singleBracketInPlaintext(
+        BracketWithContent(
+            BracketType.COMMAND,
+            " template = ${BracketType.TEMPLATE.open()}$templateContent${BracketType.TEMPLATE.close()}\n" +
+                    "template.slot = $slotContent" +
+                    "output = template.text "),
+        expected
+    )
+}
+
+//TODO: custom description
+internal fun evaluatedTemplateInPlaintext_withUnfilledSlot(templateContent: String,
+                                                           expected: String)
+        : testcase {
+
+    return evaluatedTemplateInPlaintext_withSlot(
+        templateContent,
+        liletoNullValue,
+        expected)
+}
+
+internal fun multipleSlotsInTemplate(templateContent: String,
+                                     dataToLoad: String,
+                                     expected: String,
+                                     description: List<String> = listOf("Template with multiple slots", "evaluated"))
+        : testcase {
+
+    assert(dataToLoad.indexOf(BracketType.CONTAINER.open()) == 0 &&
+            dataToLoad.indexOf(BracketType.CONTAINER.close()) == dataToLoad.length - 2) {
+        "dataToLoad should be in a container bracket" }
+
+    return singleBracketInPlaintext(
+        BracketWithContent(
+            BracketType.COMMAND,
+            " template = ${BracketType.TEMPLATE.open()}$templateContent${BracketType.TEMPLATE.close()}\n" +
+                    "< $dataToLoad\n" +
+                    "output template.text"),
+        expected,
+        description
+    )
 }
